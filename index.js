3bae81db9ba7bcd4116d0bb47f68d364c1ba4666
Toops61 <toops61@gmail.com>
2022-02-01 13:41:35 +0100

    custom button

--- index.js
+++ index.js
@@ -10,3 +10,21 @@ setTimeout(() => {
     const loader = document.querySelectorAll('.loader')[0];
     if (loader.classList.value === 'loader hidden') {loader.style.display = 'none'};
 }, 10000);
+
+function arrowAppears() {
+    document.getElementById('arrow-up').classList.add('appears');
+}
+
+function arrowUp() {
+    window.scroll(0, 0);
+}
+
+window.addEventListener('scroll', function () {
+    const arrow = document.getElementById('arrow-up');
+    if (this.scrollY > 150) {
+        arrowAppears();
+    } else {
+        arrow.classList.remove('appears');
+    }
+})
+document.getElementById('arrow-up').addEventListener('click', arrowUp);
